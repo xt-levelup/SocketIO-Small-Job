f342ddc3cae94703d9d82ab8c538504f938f776c
Phan Minh Cuong <levelup379452850@gmail.com>
2024-04-09 16:10:24 +0700

    Add more module to prepare deploy

--- server/app.js
+++ server/app.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+const path = require("path");
+const fs = require("fs");
+const helmet = require("helmet");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -6,14 +10,22 @@ const sessionStore = require("./store/sessionDB");
 const session = require("express-session");
 // const mongoDBStore = require("connect-mongodb-session")(session);
 
+const compression = require("compression");
+const morgan = require("morgan");
+
 const flash = require("connect-flash");
 
 const User = require("./models/user");
 
-const mongoDBUrl =
-    "mongodb+srv://xitrumvndn:2991981DBok@cluster0.uhljnwm.mongodb.net/shop?retryWrites=true&w=majority";
+const mongoDBUrl = process.env.MONGO_URL;
 
 const app = express();
+
+// console.log("PORT:",PORT);
+console.log("process.env.MONGO_USER:", process.env.MONGO_USER);
+console.log("process.env.MONGO_PASSWORD:", process.env.MONGO_PASSWORD);
+console.log("process.env.MONGO_URL:", process.env.MONGO_URL);
+console.log("mongoDBUrl:", mongoDBUrl);
 
 // const sessionStore = new mongoDBStore({
 //     uri: mongoDBUrl,
@@ -24,13 +36,22 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan("combined",{stream:accessLogStream}));
+
 app.use(
-    session({
-        secret: "my-secret",
-        resave: false,
-        saveUninitialized: false,
-        store: sessionStore,
-    })
+  session({
+    secret: "my-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
 );
 
 app.use(cors());
@@ -40,17 +61,17 @@ app.use(bodyParser.json());
 app.use(flash());
 
 app.use((req, res, next) => {
-    if (!req.session.user) {
-        return next();
-    }
-    User.findById(req.session.user._id)
-        .then((user) => {
-            req.user = user;
-            next();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.use(adminRoutes);
@@ -58,19 +79,20 @@ app.use(shopRoutes);
 app.use(authRoutes);
 
 mongoose
-    .connect(mongoDBUrl)
-    .then((result) => {
-        console.log("Connected!");
-        const server=app.listen(5000);
-        const io=require("./socket").init(server,{
-            cors:{
-                origin:"*"
-            }
-        });
-        io.on("connection",(socket)=>{
-            console.log("Client  connected!");
-        });
-    })
-    .catch((err) => {
-        console.log(err);
+  .connect(mongoDBUrl)
+  .then((result) => {
+    console.log("Connected!");
+    const server = app.listen(process.env.PORT || 5000);
+    // const server=app.listen(5000);
+    const io = require("./socket").init(server, {
+      cors: {
+        origin: "*",
+      },
     });
+    io.on("connection", (socket) => {
+      console.log("Client  connected!");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
